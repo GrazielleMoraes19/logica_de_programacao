2041895d93479442872dcf5bda5701cd4c72d53b
Grazielle <graziellemoraes09@gmail.com>
2025-05-29 10:57:31 -0300

    Seu_programa_js

---
+++ Seu_programa_js/Seu_programa_js.js
@@ -0,0 +1,17 @@
+//Seu_Programa_JS
+let comeco = "Seja Bem-Vindo";
+console.log(comeco);
+
+let cadastro = prompt("Digite o seu email:");
+console.log(cadastro);
+
+let confirmacao = "Confirmar";
+console.log(confirmacao);
+
+let email = prompt("Digite seu email:");
+let senha = prompt("Digite sua senha:"); // Senha como string, evite Number para senhas
+let confirmacao1 = "Confirmar senha";
+console.log(confirmacao1);
+
+let confirmacao2 = "Entrar";
+console.log(confirmacao2);
